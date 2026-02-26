cec0e73c9c8991898a97f6cd195c50dd383de461
nemas101 <nemas101@hhu.de>
2026-02-26 11:22:22 +0100

    formatted and cleaned the preprocessing

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -1,16 +1,26 @@
-import re
-from tqdm.auto import tqdm
 import argparse
+import re
+
+from tqdm.auto import tqdm
+
 
 def delete_beginning(text):
-    beginning_text_index = text.index([i for i in text if re.findall(r'\*\*\* START OF THE PROJECT GUTENBERG', i)][0])
-    text_cleared_beg = text[beginning_text_index+1:]
+    """Deletes the additional information text at the beginning provided by gutenberg.org"""
+    beginning_text_index = text.index(
+        [i for i in text if re.findall(r"\*\*\* START OF THE PROJECT GUTENBERG", i)][0]
+    )
+    text_cleared_beg = text[beginning_text_index + 1 :]
     return text_cleared_beg
 
+
 def delete_end(text):
-    end_text_index = text.index([i for i in text if re.findall(r'\*\*\* END OF THE PROJECT GUTENBERG', i)][0])
+    """Deletes the additional information text at the end provided by gutenberg.org"""
+    end_text_index = text.index(
+        [i for i in text if re.findall(r"\*\*\* END OF THE PROJECT GUTENBERG", i)][0]
+    )
     text_cleared_end = text[:end_text_index]
     return text_cleared_end
+
 
 # remove emtpy lines too?
 def remove_empty_spaces(text_lines):
@@ -19,9 +29,12 @@ def remove_empty_spaces(text_lines):
 
     return cleaned_lines
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--text', required=True, help="Name of the text in the texts_folder")
+    parser.add_argument(
+        "--text", required=True, help="Name of the text in the texts_folder"
+    )
     args = parser.parse_args()
 
     with open(f"../texts/{args.text}.txt", "r") as f:
@@ -31,5 +44,4 @@ if __name__ == "__main__":
     clean_text = delete_end(beg_text)
 
     with open(f"../texts/{args.text}_preprocessed.txt", "w") as f:
-        f.writelines(line + '\n' for line in clean_text)
-
+        f.writelines(line + "\n" for line in clean_text)
