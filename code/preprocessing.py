aff78f2f0556003c38a3c3b9e5fe87cf59e30bf6
nemas101 <nemas101@hhu.de>
2026-02-26 10:15:02 +0100

    fixed the preprocessing

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -1,30 +1,30 @@
-import re
+from tqdm.auto import tqdm
+import argparse
 
 def delete_beginning(text):
-    beginning_text = r".*(\*\*\* START OF THE PROJECT GUTENBERG EBOOK FRANKENSTEIN; OR, THE MODERN PROMETHEUS \*\*\*)"
-
-    text_cleared_beg = re.sub(beginning_text, "", text)
+    beginning_text = text.index("*** START OF THE PROJECT GUTENBERG EBOOK FRANKENSTEIN; OR, THE MODERN PROMETHEUS ***")
+    text_cleared_beg = text[beginning_text+1:]
     return text_cleared_beg
 
 def delete_end(text):
-    end_text = r"(\*\*\* END OF THE PROJECT GUTENBERG EBOOK FRANKENSTEIN; OR, THE MODERN PROMETHEUS \*\*\*).*"
-    text_cleaned_fully = re.sub(end_text, "", text)
-    return text_cleaned_fully
+    end_text = text.index("*** END OF THE PROJECT GUTENBERG EBOOK FRANKENSTEIN; OR, THE MODERN PROMETHEUS ***")
+    text_cleared_end = text[:end_text]
+    return text_cleared_end
 
 # remove emtpy lines too?
 def remove_empty_spaces(text_lines):
-    stripped_lines = [line.strip() for line in text_lines]
-    cleaned_lines = [line for line in stripped_lines if len(line) != 0]
+    stripped_lines = [line.strip() for line in tqdm(text_lines)]
+    cleaned_lines = [line for line in tqdm(stripped_lines) if len(line) != 0]
 
     return cleaned_lines
 
 if __name__ == "__main__":
     with open("../texts/frankenstein_shelley.txt", "r") as f:
         text = f.readlines()
-        cleaned_lines = remove_empty_spaces(text)
-        text = " ".join(cleaned_lines)
-        beg_text = delete_beginning(text)
-        breakpoint()
-        clean_text = delete_end(beg_text)
-        breakpoint()
+    cleaned_lines = remove_empty_spaces(text)
+    beg_text = delete_beginning(cleaned_lines)
+    clean_text = delete_end(beg_text)
+
+    with open("../texts/frankenstein_shelley_preprocessed.txt", "w") as f:
+        f.writelines(line + '\n' for line in clean_text)
 
