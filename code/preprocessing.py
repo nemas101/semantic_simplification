076f8aa1c356082365854c279fe2bf7167f9637f
nemas101 <nemas101@hhu.de>
2026-02-26 10:28:15 +0100

    made the preprocessing universal for all txt-inputs from gutenberg

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -1,14 +1,15 @@
+import re
 from tqdm.auto import tqdm
 import argparse
 
 def delete_beginning(text):
-    beginning_text = text.index("*** START OF THE PROJECT GUTENBERG EBOOK FRANKENSTEIN; OR, THE MODERN PROMETHEUS ***")
-    text_cleared_beg = text[beginning_text+1:]
+    beginning_text_index = text.index([i for i in text if re.findall(r'\*\*\* START OF THE PROJECT GUTENBERG', i)][0])
+    text_cleared_beg = text[beginning_text_index+1:]
     return text_cleared_beg
 
 def delete_end(text):
-    end_text = text.index("*** END OF THE PROJECT GUTENBERG EBOOK FRANKENSTEIN; OR, THE MODERN PROMETHEUS ***")
-    text_cleared_end = text[:end_text]
+    end_text_index = text.index([i for i in text if re.findall(r'\*\*\* END OF THE PROJECT GUTENBERG', i)][0])
+    text_cleared_end = text[:end_text_index]
     return text_cleared_end
 
 # remove emtpy lines too?
@@ -19,12 +20,16 @@ def remove_empty_spaces(text_lines):
     return cleaned_lines
 
 if __name__ == "__main__":
-    with open("../texts/frankenstein_shelley.txt", "r") as f:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--text', required=True, help="Name of the text in the texts_folder")
+    args = parser.parse_args()
+
+    with open(f"../texts/{args.text}.txt", "r") as f:
         text = f.readlines()
     cleaned_lines = remove_empty_spaces(text)
     beg_text = delete_beginning(cleaned_lines)
     clean_text = delete_end(beg_text)
 
-    with open("../texts/frankenstein_shelley_preprocessed.txt", "w") as f:
+    with open(f"../texts/{args.text}_preprocessed.txt", "w") as f:
         f.writelines(line + '\n' for line in clean_text)
 
