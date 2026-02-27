e1938f34745bdc1f2538c7739b213932d01bb7c4
nemas101 <nemas101@hhu.de>
2026-02-27 16:46:34 +0100

    fixed newlines problem

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -9,7 +9,7 @@ def delete_beginning(text):
     beginning_text_index = text.index(
         [i for i in text if re.findall(r"\*\*\* START OF THE PROJECT GUTENBERG", i)][0]
     )
-    text_cleared_beg = text[beginning_text_index + 1 :]
+    text_cleared_beg = text[beginning_text_index + 1:]
     return text_cleared_beg
 
 
@@ -26,9 +26,14 @@ def delete_end(text):
 def remove_empty_spaces(text_lines):
     stripped_lines = [line.strip() for line in tqdm(text_lines)]
     cleaned_lines = [line for line in tqdm(stripped_lines) if len(line) != 0]
-
     return cleaned_lines
 
+def remove_newlines(text_lines):
+    fulltext = " ".join(text_lines)
+    sentences = fulltext.split(".")
+    cleaned_sentences = [sentence.replace("\n", " ") for sentence in sentences]
+    dotted_sentences = [sentence + "." for sentence in cleaned_sentences]
+    return dotted_sentences
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -38,10 +43,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     with open(f"../texts/{args.text}.txt", "r") as f:
-        text = f.readlines()
-    cleaned_lines = remove_empty_spaces(text)
+        text_lines = f.readlines()
+
+    cleaned_lines = remove_empty_spaces(text_lines)
     beg_text = delete_beginning(cleaned_lines)
     clean_text = delete_end(beg_text)
+    clean_text = remove_newlines(clean_text)
 
     with open(f"../texts/{args.text}_preprocessed.txt", "w") as f:
-        f.writelines(line + "\n" for line in clean_text)
+        f.writelines(clean_text)
