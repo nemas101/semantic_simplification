13538e08385810e4736bccff8203d81f8721c169
nemas101 <nemas101@hhu.de>
2026-03-04 15:56:36 +0100

    added new vocablist and chunking

--- code/text_simplification.py
+++ code/text_simplification.py
@@ -37,20 +37,20 @@ def replace_with_hypernym(vocab_list, document):
     word_list = [w.text for w in document]
     # maybe lemmatize for all the verbs?
     # lemma_list = [lemmatizer.lemmatize(word) for word in word_list]
-    for index, word in enumerate(word_list):
-        lemma = lemmatizer.lemmatize(word)
-        if lemma in vocab_list:
-            synset = wn.synsets(word)
-            if len(synset) > 0:
-                hypernym_list = synset[0].hypernyms()
-                if len(hypernym_list) > 0:
-                    hypernym = hypernym_list[0]
-                    word_hypernym_synset = hypernym
-                    name = word_hypernym_synset.name()
-                    new_word = name.split(".")[0]
-                    word_list[index] = new_word    
-    document = " ".join(word_list)
-    return document
+    for word in document:
+        word_text = word.text
+        # lemma = lemmatizer.lemmatize(word_text)
+        # if lemma in vocab_list:
+        synset = wn.synsets(word_text)
+        if len(synset) > 0:
+            hypernym_list = synset[0].hypernyms()
+            if len(hypernym_list) > 0:
+                hypernym = hypernym_list[0]
+                word_hypernym_synset = hypernym
+                name = word_hypernym_synset.name()
+                new_word = name.split(".")[0]
+                cleaned_document = document.text.replace(word.text, new_word)
+    return cleaned_document
 
 
 if __name__ == "__main__":
@@ -63,15 +63,31 @@ if __name__ == "__main__":
 
     with open(f"../texts/{args.text}.txt", "r") as f:
         text = f.read()
-    df_vocab = pd.read_csv("c1c2_vocab_list.csv")
-    complex_word_list = list(df_vocab["headword"])
 
-    nlp = spacy.load("en_core_web_sm")
+    a1b2_vocab = pd.read_csv("a1b2_vocab_list.csv")
+    c1_vocab = pd.read_csv("c1c2_vocab_list.csv")
+
+    complex_word_list = list(a1b2_vocab["headword"])
+    complex_word_list.extend(c1_vocab["headword"])
+
+    nlp = spacy.load("en_core_web_sm", disable=['tagger', 'ner', 'lemmatizer', 'textcat'])
+
+    nlp.add_pipe("spacy_chunks", last=True, config={
+    "chunking_method": "sentence",
+    "chunk_size": 10,
+    "overlap": 1,
+    "truncate": True
+
+})
+
     doc = nlp(text)
 
-    spacy_fy_sentences = spacyfy_text(text)
+    # spacy_fy_sentences = spacyfy_text(text)
 
     hypernym_doc = replace_with_hypernym(complex_word_list, doc)
-    breakpoint()
+
+    with open(f"../texts/{args.text}_simplified.txt", "w") as t:
+        t.write(hypernym_doc)
+        
     # wordnet_dict = wordnet_tokenization(text)
 
